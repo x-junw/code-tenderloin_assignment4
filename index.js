935053983238cd608ba8bf0326432be677dfb048
xjunw <awesomejay180@gmail.com>
2026-07-20 13:53:04 -0700

    Finished Base Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = async () => {
+    document.querySelectorAll(".container").forEach((element) => {
+        for (let i = 1; i <= 14; i++) {
+            let newItem = document.createElement("div");
+            newItem.className = "item";
+            newItem.id = `item${i}`
+            element.appendChild(newItem);
+        }
+    });
+}
